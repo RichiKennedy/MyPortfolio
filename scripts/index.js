47855b6eec9c89548538678ad0d1939735d2d746
RichiKennedy <richard.kennedy@hyperisland.se>
2022-02-27 19:26:47 +0100

    next stage

--- scripts/index.js
+++ scripts/index.js
@@ -63,3 +63,22 @@ killerBtn.onclick = function () {
 kGoBack.onclick = function () {
   killerSlideModal.style.display = "none";
 };
+
+// Get the modal
+let zodiacSlideModal = document.getElementById("zodiac");
+
+// Get the button that opens the modal
+let zodiacBtn = document.getElementById("zodiacBtn");
+
+// Get the <span> element that closes the modal
+let zGoBack = document.getElementsByClassName("zBack-to-projects")[0];
+
+// When the user clicks the button, open the modal
+zodiacBtn.onclick = function () {
+  zodiacSlideModal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+zGoBack.onclick = function () {
+  zodiacSlideModal.style.display = "none";
+};
